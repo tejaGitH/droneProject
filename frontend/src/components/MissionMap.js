b9a0a738e35b3a6d3a503691f25804b782d4a526
tejaGitH <navyateja.ch@outlook.com>
2025-03-29 14:43:58 +0530

    enhanced map view for the given Mission and co-ordinates

--- frontend/src/components/MissionMap.js
+++ frontend/src/components/MissionMap.js
@@ -1,6 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
 import { useSelector } from 'react-redux';
+import L from 'leaflet';
+
+const colors = ['red', 'blue', 'green', 'orange', 'purple', 'pink', 'cyan'];
+
+const getColor = (index) => colors[index % colors.length];
+
+const createCustomIcon = (color) => {
+    return L.divIcon({
+        html: `<i class="fas fa-map-marker-alt" style="color: ${color}; font-size: 24px;"></i>`,
+        iconSize: [24, 24],
+        className: 'custom-marker',
+    });
+};
 
 const MissionMap = () => {
     const missions = useSelector((state) => state.missions.missions);
@@ -20,13 +33,36 @@ const MissionMap = () => {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; OpenStreetMap contributors'
                 />
-                {mapData && mapData.flatMap((mission) => (
-                    mission.coordinates.map((coord, index) => (
-                        <Marker key={`${mission._id}-${index}`} position={[coord.lat, coord.lng]}>
-                            <Popup>{`${mission.name} - (${coord.lat}, ${coord.lng})`}</Popup>
-                        </Marker>
-                    ))
-                ))}
+                {mapData && mapData.map((mission, missionIndex) => {
+                    const color = getColor(missionIndex);
+
+                    // Extracting coordinates
+                    const coordinates = mission.coordinates.map((coord) => [coord.lat, coord.lng]);
+
+                    return (
+                        <React.Fragment key={mission._id}>
+                            {coordinates.length > 1 && (
+                                <Polyline
+                                    pathOptions={{ color: color, weight: 4 }}
+                                    positions={coordinates}
+                                />
+                            )}
+                            {coordinates.map((position, index) => (
+                                <Marker
+                                    key={`${mission._id}-${index}`}
+                                    position={position}
+                                    icon={createCustomIcon(color)}
+                                >
+                                    <Popup>
+                                        <strong>{mission.name}</strong><br />
+                                        Latitude: {position[0]}<br />
+                                        Longitude: {position[1]}
+                                    </Popup>
+                                </Marker>
+                            ))}
+                        </React.Fragment>
+                    );
+                })}
             </MapContainer>
         </div>
     );
